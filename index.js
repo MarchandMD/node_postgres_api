d34816980beca5f1e0af0ac2a4c8ed6f637621bb
Michael Marchand <michaeldavidmarchand@gmail.com>
2023-02-28 09:43:25 -0700

    Removes comments

--- index.js
+++ index.js
@@ -1,24 +1,16 @@
-// require the express package
 const express = require('express')
-// require the body-parser middleware
 const bodyParser = require('body-parser')
-// invoke express
 const app = express()
-// set the port
 const port = 3000
-// this will import all from queries.js
 const db = require('./queries')
 
-// the app constant has access to a #use function that accepts the middleware, which accepts a #json function
 app.use(bodyParser.json())
-// the app constant us called again with #use, again the bodyParser middleware is called, this time with #urlencoded, and that's receiving the key/value pair of extended: true
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
 )
 
-// if the app gets a GET request with the HTTP path of / (so, the root), respond with some JSON info
 app.get('/', (request, response) => {
   response.json({
     info: 'Node.js, Express and Postgres API',
@@ -31,7 +23,6 @@ app.get('/', (request, response) => {
   })
 })
 
-// if you get this, do this; I'm guessing there's also an app.post function
 app.get('/users', db.getUsers)
 app.get('/users/:id', db.getUserById)
 app.post('/users', db.createUser)
--- queries.js
+++ queries.js
@@ -7,24 +7,15 @@ const pool = new Pool({
   port: 5432
 })
 
-// function to be used when an HTTP GET request arrives at the server
 const getUsers = (request, response) => {
-  // go to the database via the pool, and #query it by running the first argument
-  // it appears the #query function accepts a second argument of a function
-  // the second function includes (errors, results), which arrive from the #query function performing some task on the first argument
-  // the first argument no doubt can be nil/null, since it's not always expected to result in an error
-  // the second argument is the results of the query
-  // those two arguments are passed to the body of the arrow function
   pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
     if (error) {
       throw error
     }
-    // this is interesting....so the keyword response has a #status function which allows a status code to be specified, accepts a #json function which allows the second argument of this function to be serialized
     response.status(200).json(results.rows)
   })
 }
 
-// constant/function to use for GET request to /users/:id
 const getUserById = (req, res) => {
   const id = parseInt(req.params.id)
 
@@ -36,7 +27,6 @@ const getUserById = (req, res) => {
   })
 }
 
-// constant/function to use when getting a POST request to /users
 const createUser = (req, res) => {
   const { name, email } = req.body
 
@@ -49,7 +39,6 @@ const createUser = (req, res) => {
   })
 }
 
-// constant/function to use when getting a POST request for /users/:id
 const updateUser = (req, res) => {
   const id = parseInt(req.params.id)
   const { name, email } = req.body
@@ -66,7 +55,6 @@ const updateUser = (req, res) => {
   )
 }
 
-// constant/function to use when getting a DELETE request for /users/:id
 const deleteUser = (req, res) => {
   const id = parseInt(req.params.id)
 
@@ -78,7 +66,6 @@ const deleteUser = (req, res) => {
   })
 }
 
-// this is how these functions are "packaged" and exposed in scope to something else, specifically the index.js
 module.exports = {
   getUsers,
   getUserById,
